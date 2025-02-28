29bd344a950f4efaf9aed8ef21e4ed9ff4ae279d
aimensiddiqui97 <aimensiddiqui97@gmail.com>
2025-02-27 21:38:23 -0500

    Update search.php

--- search.php
+++ search.php
@@ -1,31 +1,41 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
+// Connect to the database
+$conn = new mysqli("Users/aimenusuf/Downloads/Untitled.sqlite", "aimensiddiqui97@gmail.com", "@Siddiqui97", "Untitled.sqlite");
 
-$conn = new mysqli("/Users/aimenusuf/Downloads/Untitled.sqlite", "aimensiddiqui97@gmail.com", "@Siddiqui97", "healthrates_db");
-
+// Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+  die("Connection failed: " . $conn->connect_error);
 }
 
-$cpt = isset($_GET['cpt']) ? $_GET['cpt'] : '';
-$hospital = isset($_GET['hospital']) ? $_GET['hospital'] : '';
+// Get search parameters
+$cpt = $_GET['cpt'] ?? '';
+$hospital = $_GET['hospital'] ?? '';
 
-$sql = "SELECT * FROM procedures WHERE 1=1";
-
+// Build the SQL query
+$sql = "SELECT hospital, cpt, description, price FROM procedures WHERE 1=1";
 if (!empty($cpt)) {
-    $sql .= " AND cpt_code LIKE '%$cpt%'";
+  $sql .= " AND cpt LIKE '%$cpt%'";
 }
 if (!empty($hospital)) {
-    $sql .= " AND hospital LIKE '%$hospital%'";
+  $sql .= " AND hospital LIKE '%$hospital%'";
 }
 
+// Fetch filtered data
 $result = $conn->query($sql);
-$data = [];
 
-while ($row = $result->fetch_assoc()) {
+$data = [];
+if ($result->num_rows > 0) {
+  while ($row = $result->fetch_assoc()) {
     $data[] = $row;
+  }
 }
+
+// Return data as JSON
+header('Content-Type: application/json');
+echo json_encode($data);
+
+$conn->close();
+?>
 
 echo json_encode($data);
 $conn->close();

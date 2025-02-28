f4fe2c93917557cf7af94c68648a317792cd2af7
aimensiddiqui97 <aimensiddiqui97@gmail.com>
2025-02-27 21:37:18 -0500

    Create getData.php

---
+++ getData.php
@@ -0,0 +1,26 @@
+<?php
+// Connect to the database
+$conn = new mysqli("Users/aimenusuf/Downloads/Untitled.sqlite", "aimensiddiqui97@gmail.com", "@Siddiqui97", "Untitled.sqlite");
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Fetch all data
+$sql = "SELECT hospital, cpt, description, price FROM procedures";
+$result = $conn->query($sql);
+
+$data = [];
+if ($result->num_rows > 0) {
+  while ($row = $result->fetch_assoc()) {
+    $data[] = $row;
+  }
+}
+
+// Return data as JSON
+header('Content-Type: application/json');
+echo json_encode($data);
+
+$conn->close();
+?>

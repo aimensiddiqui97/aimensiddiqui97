5711e21a153ba65214ce2aa289348c118e8d83bf
aimensiddiqui97 <aimensiddiqui97@gmail.com>
2025-02-27 21:00:44 -0500

    Create search.php

---
+++ search.php
@@ -0,0 +1,32 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+$conn = new mysqli("localhost", "your_user", "your_password", "healthrates_db");
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$cpt = isset($_GET['cpt']) ? $_GET['cpt'] : '';
+$hospital = isset($_GET['hospital']) ? $_GET['hospital'] : '';
+
+$sql = "SELECT * FROM procedures WHERE 1=1";
+
+if (!empty($cpt)) {
+    $sql .= " AND cpt_code LIKE '%$cpt%'";
+}
+if (!empty($hospital)) {
+    $sql .= " AND hospital LIKE '%$hospital%'";
+}
+
+$result = $conn->query($sql);
+$data = [];
+
+while ($row = $result->fetch_assoc()) {
+    $data[] = $row;
+}
+
+echo json_encode($data);
+$conn->close();
+?>
